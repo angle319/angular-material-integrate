72bb94de8e2d66444803d7bd001011082c941a2e
angle <angle319angle319@gmail.com>
2019-07-11 17:20:15 +0800

    modify page

--- library/material-lib/src/lib/page/page.module.ts
+++ library/material-lib/src/lib/page/page.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginPageComponent } from './login.component';
+import { LoginPageComponent } from './login/login.component';
 import { MaterialModule } from '../shared/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeCommonModule } from '../shared/fontawesome.module';
